c0319fc3a018728b01733adcf0ad996f3b5a37d6
wochichaofan <ia.serdyukov@yandex.ru>
2024-07-24 17:16:37 +0300

    Fixed ProbTheory files

--- ProbTheory/mathprob.py
+++ ProbTheory/mathprob.py
@@ -61,3 +61,44 @@ class Stats_Functions:
         else:
             lmbd = n*p
             return float(Decimal(np.math.pow(lmbd, m) / fact(m)) * Decimal(np.exp(-lmbd)))
+        
+        
+class Distribution_funcs(Stats_Functions):
+    def __init__(self):
+        super().__init__()
+        
+    def geometric_destribution(self, n, p):
+        q = round(1-p, 4)
+
+        d = {xi: round(q**(xi-1) * p, 4) for xi in range(1, n)}    
+        d[n] = round(q**(n-1), 4)
+
+        assert sum(d.values()) == 1, 'sum not equal to 1, check input data'
+        return d
+
+    def binominal_destribution(self, m, n, p):
+        q = (1-Decimal(p))
+
+        return {i : self.Bernoulli_prob(i, n, p, q) for i in range(m+1)}
+
+    def Poisson_dist(n=None, p=None, lmbd=None):
+        if lmbd == None:
+            assert n*p >= 0, 'Poisson can\'t be used: lambda is less than 0'
+            lmbd = n*p
+            return {xi : self.Poisson_prob(xi, n, lmbd=lmbd) for xi in range(n)}
+        else:
+            return {xi : self.Poisson_prob(xi, n, lmbd=lmbd) for xi in range(n)}
+
+    def hyper_geometry_dist(N, M, n, x_exp=None):
+        """
+        N - кол-во объектов в совокупности
+        M - кол-во объектов искомого класса
+        n - размер выборки
+        x_exp - размер максимального ожидаемого кол-ва объектов в выборке
+        """
+
+        x_exp = n if x_exp is None or x_exp < n else x_exp
+        x_exp = M if M < x_exp else x_exp
+
+        d = {xi: round(Decimal((C(xi, M) * C(n-xi, N-M)) / C(n, N)), 4) for xi in range(0, x_exp+1)}
+        return d
